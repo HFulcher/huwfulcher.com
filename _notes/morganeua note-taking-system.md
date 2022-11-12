94b0752b85a6379f340f30f55719ca37dd37e335
Huw Fulcher <fulcherhuw@gmail.com>
2022-11-12 22:34:05 +0000

    Cleaning dead files

--- _notes/morganeua note-taking-system.md
+++
@@ -1,8 +0,0 @@
----
----
-
-Link: https://www.youtube.com/watch?v=L9SLlxaEEXY
-
-# The FUN and EFFICIENT note-taking system I use in my PhD
-
-![[taking-notes-with-obsidian]]
--- move_notes.sh
+++ move_notes.sh
@@ -6,6 +6,10 @@ OBSIDIAN_NOTES_PATH="/Users/huwfulcher/Documents/Huw's Sync Vault"
 echo "$JEKYLL_NOTES_PATH/"
 echo "$OBSIDIAN_NOTES_PATH/"
 
+echo "Deleting old notes"
+
+rm "${JEKYLL_NOTES_PATH}"/*
+
 echo "Moving notes from $OBSIDIAN_NOTES_PATH to $JEKYLL_NOTES_PATH"
 
 cp -a "$OBSIDIAN_NOTES_PATH/" "$JEKYLL_NOTES_PATH/"
